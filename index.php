b5d5aaab23caef113ed5c1faa6bb98986e32a74b
Dawid Klimowicz <dawidkl777@gmail.com>
2022-11-29 19:18:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+$thisPage = "Dashboard"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+
+</head>
+<body>
+    <?php include "navigation.php" ?>
+  
+        <div class="say-hi">
+            <text>
+                Witaj, Dawid 👋
+            </text>
+            <img src="https://i.pinimg.com/236x/4e/45/88/4e458893b1fdc033508016e09fa5553c.jpg" alt="avatar" class="avatar">
+
+            <button>
+                <i class="material-symbols-outlined">notifications</i>
+            </button>
+            
+        </div>
+</body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,26 @@
+<?php  $selected = false?>
+
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+    <div class="header">
+        <div class="side-nav">
+            <a href="#" class="logo">
+                <text class="inlogo">
+                    Factur.io
+                </text>
+                <text class="logo-icon">F</text>
+
+            </a>
+            <ul class="nav-links">
+                <li ><a href="index.php"><i class="material-symbols-outlined">home</i><p>Panel Główny</p></a></li>
+                <li><a href="#"> <i class="material-symbols-outlined">history</i><p>Historia</p></a></li>
+                <li><a href="#"> <i class="material-symbols-outlined">inventory_2</i> <p>Produkty</p></a></li>
+                <li><a href="#"> <i class="material-symbols-outlined">group</i> <p>Odbiorcy</p></a></li>
+                <li><a href="#"> <i class="material-symbols-outlined">request_page</i><p>Nowa faktura</p></a></li>
+                <div class="active"></div>
+            </ul>
+        </div>
+        
+    </div>
+
