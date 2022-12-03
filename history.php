b6b9487edf19efa7a37e9c377bcf13272d79096d
Dawid Klimowicz <dawidkl777@gmail.com>
2022-12-03 14:50:30 +0100

    test commit

--- history.php
+++ history.php
@@ -9,9 +9,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style_d.css">
+
+
 </head>
 <body>
     <h1>HISTORY</h1>
     <?php include 'navigation.php'?>
+    <div class="page">
+
+        <text class="test">Historia </text>>
+    </div>
 </body>
 </html>
