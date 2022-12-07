4ffb98d5498dc17069b13aefcabdbe12f674765c
Dawid Klimowicz <dawidkl777@gmail.com>
2022-12-07 18:06:43 +0100

    history page

--- history.php
+++ history.php
@@ -18,7 +18,42 @@
     <?php include 'navigation.php'?>
     <div class="page">
         <div class="say-hi">
+            <div class="history_header">
         <text> Historia </text>
+   
+        <div class="time_button">
+                <text>
+                    Month
+                </text>
+                <i class="material-symbols-outlined">arrow_drop_down</i>
+
+            </div>
+
+        </div>
+
+        </div>
+        <div class="transactions_row">
+            <div class="company_icon">
+            <i class="material-symbols-outlined">attach_money</i>
+            </div>
+            <text class="company_name"> Dawid Corporantion SA</text>
+            <text class="company_date">22 Nov 2022</text>
+            <text class="company_name"> $ 2,700</text>
+            <div class="status_paid">
+                <text class="status">Opłacona</text>
+            </div>
+            <div class="menage_buttons">
+            <div class="info_icon">
+            <i class="material-symbols-outlined">info</i>
+            </div>
+            <div class="edit_icon">
+            <i class="material-symbols-outlined">edit</i>
+            </div>
+            <div class="delete_icon">
+            <i class="material-symbols-outlined">delete</i>
+            </div>
+            </div>
+
         </div>
     </div>
 </body>
