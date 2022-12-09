88731b2ea5d8bbfc2aaed5fe8a73029c0876d938
Magdalena Osinska <osinska.magdalenamaria@gmail.com>
2022-12-09 14:45:43 +0100

    basic update

--- products.php
+++ products.php
@@ -9,9 +9,90 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style_m.css">
 </head>
+
 <body>
-    <h1>PRODUCTS</h1>
-    <?php include 'navigation.php'?>
+<?php include 'navigation.php'?>
+    <div class="page">
+        <div class="say-hi">
+            <div class="history_header">
+        <text> Produkty </text>
+   
+        <div class="time_button">
+                <text>
+                    Dodaj
+                </text>
+                <i class="material-symbols-outlined">arrow_drop_down</i>
+
+            </div>
+
+        </div>
+
+        </div>
+
+        <div class="products_row">
+            <div class="company_icon">
+            <i class="material-symbols-outlined">inventory_2</i>
+            </div>
+            <text class="product_name"> Jakiś produkt 1</text>
+            <text class="prod_description">Opis produktu</text>
+            <text class="product_name"> $ 2,700</text>
+            <div class="menage_buttons">
+            <div class="info_icon">
+            <i class="material-symbols-outlined">info</i>
+            </div>
+            <div class="edit_icon">
+            <i class="material-symbols-outlined">edit</i>
+            </div>
+            <div class="delete_icon">
+            <i class="material-symbols-outlined">delete</i>
+            </div>
+            </div>
+
+        </div>
+
+        <div class="products_row">
+            <div class="company_icon">
+            <i class="material-symbols-outlined">inventory_2</i>
+            </div>
+            <text class="product_name"> Jakiś produkt 2</text>
+            <text class="prod_description">Opis produktu</text>
+            <text class="product_name"> $ 2,700</text>
+            <div class="menage_buttons">
+            <div class="info_icon">
+            <i class="material-symbols-outlined">info</i>
+            </div>
+            <div class="edit_icon">
+            <i class="material-symbols-outlined">edit</i>
+            </div>
+            <div class="delete_icon">
+            <i class="material-symbols-outlined">delete</i>
+            </div>
+            </div>
+
+        </div>
+
+        <div class="products_row">
+            <div class="company_icon">
+            <i class="material-symbols-outlined">inventory_2</i>
+            </div>
+            <text class="product_name"> Jakiś produkt 3</text>
+            <text class="prod_description">Opis produktu</text>
+            <text class="product_name"> $ 2,700</text>
+            <div class="menage_buttons">
+            <div class="info_icon">
+            <i class="material-symbols-outlined">info</i>
+            </div>
+            <div class="edit_icon">
+            <i class="material-symbols-outlined">edit</i>
+            </div>
+            <div class="delete_icon">
+            <i class="material-symbols-outlined">delete</i>
+            </div>
+            </div>
+
+        </div>
+    </div>
 </body>
 </html>
